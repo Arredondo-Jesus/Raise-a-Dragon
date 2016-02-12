8aada6fb4ea20a80f1ba4b8143999221efe670fb
Chuy <Chuy@Chuy-PC>
2016-02-11 23:51:11 -0600

    2-11-16 creation of the control layer classes and coding of the controlInventory class

--- RaiseADragon/src/byui/cit260/raiseADragon/control/controlInventory.java
+++ RaiseADragon/src/byui/cit260/raiseADragon/control/controlInventory.java
@@ -22,26 +22,31 @@ public class controlInventory {
     
     public double calcCostOfItem(int number, double unitCost, Item item, String nameOfItem) {
         
-        
+      //Checks if the received item object is not an empty object  
         if(item==null){
             return -1;
         }
         
+        //Compares the name attribute of the received object with a valid item name
         if(!item.getName().equals(nameOfItem)){
             return -1;
         }
         
+        //Returns an error if the number of items are 0
         if (number < 0){
             return -1;
         }
         
+        //Validates if the unit cost is not negative and if it not greater than 9999
         if(unitCost < 0 || unitCost > 9999){
             return -1;
         }
         
+        // Calculates total cost
         double totalCost;
         totalCost = totalCost =+ (unitCost * number);
         
+        //Returns the totalCost of the items
         return totalCost;
     }
     
