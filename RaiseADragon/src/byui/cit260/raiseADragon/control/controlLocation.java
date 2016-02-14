18658302b17e0000ac9544e9631702da7e70fed8
John <John@10.0.0.6>
2016-02-13 21:44:41 -0700

    2/13/16 9:43PM Implemented the code for controlLocation, and created controlLocationTest in byui.cit260.raiseADragon.control

--- RaiseADragon/src/byui/cit260/raiseADragon/control/controlLocation.java
+++ RaiseADragon/src/byui/cit260/raiseADragon/control/controlLocation.java
@@ -4,11 +4,45 @@
  * and open the template in the editor.
  */
 package byui.cit260.raiseADragon.control;
-
+import byui.cit260.raiseADragon.model.Location;
 /**
  *
  * @author Chuy
  */
 public class controlLocation {
     
+    /**
+     *
+     * @param locationNumber
+     * @param positionInMap
+     * @param name
+     * @param newLocation
+     * @return
+     */
+
+    
+    public double moveToLocation(int locationNumber, int positionInMap, Location name, String newLocation) {
+        
+        //Find out if the selected location is null
+        if(newLocation==null){
+            return -1;
+        }
+        
+        //Find out if the current location is the same as the destination input
+        if(locationNumber==positionInMap){
+            return -1;
+        }
+        
+        //Find out if the location is within the parameters of places to go
+        if(locationNumber < 0 || locationNumber > 13){
+            return -1;
+            
+        }
+        
+        //Create a dependant variable to act as a return value.
+        double newMapPosition;
+        newMapPosition = locationNumber;
+        
+        return newMapPosition;
+    }
 }
---
+++ RaiseADragon/test/byui/cit260/raiseADragon/control/controlLocationTest.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.raiseADragon.control;
+
+import byui.cit260.raiseADragon.model.Location;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author John
+ */
+public class controlLocationTest {
+    
+    public controlLocationTest() {
+    }
+
+    /**
+     * Test of moveToLocation method, of class controlLocation.
+     */
+    @Test
+    public void testMoveToLocation() {
+        
+        System.out.println("\ttestMoveToLocation");
+        
+        /*********************************
+         * Test case #1
+        **********************************/
+                
+        System.out.println("\tTest 1");
+        
+        //Get Variables
+        int locationNumber = 1;
+        int positionInMap = 2;
+        Location name = new Location();
+        name.setName("Desert");
+        String newLocation = "Desert";
+        
+        //Create a new instance of controlLocation
+        controlLocation instance = new controlLocation();
+        
+        //Expected results versus actual results
+        double expResult = 1;
+        double result = instance.moveToLocation(locationNumber, positionInMap, name, newLocation);
+        
+        //check the validity of the test
+        assertEquals(expResult, result, 0);
+        
+        
+        /*********************************
+         * Test case #2
+        **********************************/
+                
+        System.out.println("\tTest 2");
+        
+        //Get Variables
+        locationNumber = 1;
+        positionInMap = 2;
+        name = new Location();
+        name.setName("Desert");
+        newLocation = null;
+        
+        //Expected results versus actual results
+        expResult = -1;
+         result = instance.moveToLocation(locationNumber, positionInMap, name, newLocation);
+        
+        //check the validity of the test
+        assertEquals(expResult, result, 0);
+        
+        
+                /*********************************
+         * Test case #3
+        **********************************/
+                
+        System.out.println("\tTest 3");
+        
+        //Get Variables
+        locationNumber = 1;
+        positionInMap = 1;
+        name = new Location();
+        name.setName("Desert");
+        newLocation = "Desert";
+        
+        //Expected results versus actual results
+        expResult = -1;
+         result = instance.moveToLocation(locationNumber, positionInMap, name, newLocation);
+        
+        //check the validity of the test
+        assertEquals(expResult, result, 0);
+        
+        
+                /*********************************
+         * Test case #4
+        **********************************/
+                
+        System.out.println("\tTest 4");
+        
+        //Get Variables
+        locationNumber = -1;
+        positionInMap = 2;
+        name = new Location();
+        name.setName("Desert");
+        newLocation = "Desert";
+        
+        //Expected results versus actual results
+        expResult = -1;
+         result = instance.moveToLocation(locationNumber, positionInMap, name, newLocation);
+        
+        //check the validity of the test
+        assertEquals(expResult, result, 0);
+        
+        
+                /*********************************
+         * Test case #5
+        **********************************/
+                
+        System.out.println("\tTest 5");
+        
+        //Get Variables
+        locationNumber = 0;
+        positionInMap = 13;
+        name = new Location();
+        name.setName("Desert");
+        newLocation = "Desert";
+        
+        //Expected results versus actual results
+        expResult = 0;
+         result = instance.moveToLocation(locationNumber, positionInMap, name, newLocation);
+        
+        //check the validity of the test
+        assertEquals(expResult, result, 0);
+        
+        
+                /*********************************
+         * Test case #6
+        **********************************/
+                
+        System.out.println("\tTest 6");
+        
+        //Get Variables
+        locationNumber = 13;
+        positionInMap = 0;
+        name = new Location();
+        name.setName("Desert");
+        newLocation = "Desert";
+        
+        //Expected results versus actual results
+        expResult = 13;
+         result = instance.moveToLocation(locationNumber, positionInMap, name, newLocation);
+        
+        //check the validity of the test
+        assertEquals(expResult, result, 0);
+        
+        
+                /*********************************
+         * Test case #7
+        **********************************/
+                
+        System.out.println("\tTest 7");
+        
+        //Get Variables
+        locationNumber = 13;
+        positionInMap = 13;
+        name = new Location();
+        name.setName("Desert");
+        newLocation = "Desert";
+        
+        //Expected results versus actual results
+        expResult = -1;
+         result = instance.moveToLocation(locationNumber, positionInMap, name, newLocation);
+        
+        //check the validity of the test
+        assertEquals(expResult, result, 0);
+    }
+    
+}
