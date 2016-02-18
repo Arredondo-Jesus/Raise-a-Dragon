6197e357af05c2782f0e85b8707332927777e6c9
Chuy <Chuy@Chuy-PC>
2016-02-17 22:48:07 -0600

    2-13-17 Changed title of the Help Menu from Game Menu to Help Menu

--- RaiseADragon/src/byui/cit260/raiseADragon/view/MainMenuView.java
+++ RaiseADragon/src/byui/cit260/raiseADragon/view/MainMenuView.java
@@ -119,7 +119,7 @@ public class MainMenuView {
         while(!valid) { //while a valid name has not been retrieved
                 
                 //prompt for the player's name
-                System.out.println("Enter the option below:");
+                System.out.println("\n\nEnter the option below:");
                 
                 // get the name from the keyboard and trim off the blanks
                 playersSelection = keyboard.nextLine();
